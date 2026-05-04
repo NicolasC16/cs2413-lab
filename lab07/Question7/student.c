696f1c35aa5470724900e82369e466b8abd13c03
Nicolas Capetillo <nicapeti@ttu.edu>
2026-05-03 23:11:14 -0500

    Finish lab07

--- lab07/Question7/student.c
+++ lab07/Question7/student.c
@@ -1,35 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-Hybrid Merge Sort Lab
-
-Objective:
-Implement a hybrid sorting algorithm that combines merge sort and insertion sort.
-
-Idea:
-- Merge sort has worst-case time complexity O(n log n).
-- Insertion sort has worst-case time complexity O(n^2), but it is fast for small subarrays.
-- Therefore, when the current subarray size is less than or equal to k,
-  use insertion sort instead of continuing recursive merge sort.
-
-Input array:
-[39, 9, 81, 45, 90, 27, 72, 18]
-
-Expected sorted output:
-[9, 18, 27, 39, 45, 72, 81, 90]
-
-Students need to complete:
-
-1. insertionSort
-2. merge
-3. hybridMergeSort
-*/
-
-/*
-Print the array.
-Do not modify this function.
-*/
 void printArray(int arr[], int size) {
     printf("[");
     for (int i = 0; i < size; i++) {
@@ -42,10 +13,7 @@ void printArray(int arr[], int size) {
     printf("]\n");
 }
 
-/*
-Check whether the array is sorted.
-Do not modify this function.
-*/
+
 int isSorted(int arr[], int size) {
     for (int i = 1; i < size; i++) {
         if (arr[i - 1] > arr[i]) {
@@ -55,39 +23,54 @@ int isSorted(int arr[], int size) {
     return 1;
 }
 
-/*
-Sort arr[left...right] using insertion sort.
-*/
+
 void insertionSort(int arr[], int left, int right) {
-    // TODO: implement insertion sort for arr[left...right]
+    for (int i = left + 1; i <= right; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while ( j >= left && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
 }
 
-/*
-Merge two sorted subarrays into one sorted subarray.
-*/
 void merge(int arr[], int left, int mid, int right) {
-    // TODO: implement merge operation
+    int L[100], R[100];
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
+
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
 }
 
-/*
-Hybrid merge sort.
-*/
 void hybridMergeSort(int arr[], int left, int right, int k) {
-    // TODO: implement hybrid merge sort
+    if (right - left + 1 <= k) {
+        insertionSort(arr, left, right);
+        return;
+    }
+
+    int mid = (left + right) / 2;
+    hybridMergeSort(arr, left, mid, k);
+    hybridMergeSort(arr, mid + 1, right, k);
+    merge(arr, left, mid, right);
 }
 
 int main() {
     int arr[] = {39, 9, 81, 45, 90, 27, 72, 18};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    /*
-    k is the threshold.
-
-    When the current subarray size is less than or equal to k,
-    insertion sort should be used.
-
-    You may change k to test different behaviors.
-    */
     int k = 3;
 
     printf("Original array:\n");
